757bf4677aab107c131fd8aa1659b1af2573a37e
Chris Romack <chris.romack@invitationhomes.com>
2023-05-27 11:59:14 -0500

    feat: testing out a composite action

--- src/__mocks__/@actions/core.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  debug: jest.fn(),
-  getInput: jest.fn(),
-  setFailed: jest.fn(),
-  setOutput: jest.fn(),
-};
--- src/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import * as run from '../run';
-
-jest.mock('../run');
-
-it('should run the action once', () => {
-  require('../');
-  expect(run.default).toHaveBeenCalledTimes(1);
-});
